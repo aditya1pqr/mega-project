84a29258380ac11fd1046c3810da89b7d3e6b4c7
Aditya <aadi099012@gmail.com>
2023-10-06 22:08:11 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  return(
+  
+  <BrowserRouter>
+  <Routes>
+  <Route path='/' element={<Home/>}></Route>
+  <Route path='/sign-in' element={<SignIn/>}/>
+  <Route path='/sign-up' element={<SignUp/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/profile' element={<Profile/>}/>
+  </Routes>
+  
 
-  return (
-    <>
-      <div className='text-red-500'>hellow</div>
-    </>
+  </BrowserRouter>
   )
+  
 }
-
-export default App
